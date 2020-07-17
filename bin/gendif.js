acfba7f77f1badc5bb33161f40e33fc9d7d59be6
O Melchukova <rr26@mail.ru>
2020-07-17 18:34:25 +0300

    Step 2

--- bin/gendif.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import commander from '../src/gendif.js';
-
-commander();
--- src/commander.js
+++ src/commander.js
@@ -6,8 +6,8 @@ export default () => {
     .description('Compares two configuration files and shows a difference.')
     .version(version, '-V, --version',  'output the version number')
     .helpOption('-h, --help', 'output usage information')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>');
 
   program.parse(process.argv);
 };
-
-//export { commander };
