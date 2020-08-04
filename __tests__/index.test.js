b143de46e120037cc48205eb5f09be392f6a1c15
O Melchukova <rr26@mail.ru>
2020-08-04 22:41:31 +0300

    Carry over and rename constants

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,26 +8,24 @@ const getFile = (name) => {
   return getParsedFileContent(path);
 };
 
-describe('compare files', () => {
-  const resultFormats = ['stylish', 'plain', 'json'];
-  const resultExtensions = ['json', 'yml', 'ini'];
+const outputFormats = ['stylish', 'plain', 'json'];
+const inputFormats = ['json', 'yml', 'ini'];
 
-  const formatTbl = resultFormats.map((format) => {
-    const result = getFile(`${format}Result.txt`);
-    return [format, result];
-  });
+const formatTbl = outputFormats.map((format) => {
+  const result = getFile(`${format}Result.txt`);
+  return [format, result];
+});
 
-  const extTbl = resultExtensions.map((ext) => {
-    const file1Path = getFixturePath(`file1.${ext}`);
-    const file2Path = getFixturePath(`file2.${ext}`);
-    return [ext, file1Path, file2Path];
-  });
+const extTbl = inputFormats.map((ext) => {
+  const file1Path = getFixturePath(`file1.${ext}`);
+  const file2Path = getFixturePath(`file2.${ext}`);
+  return [ext, file1Path, file2Path];
+});
 
-  describe.each(formatTbl)('print format: %s', (format, result) => {
-    test.each(extTbl)('files extension: %s', (ext, file1Path, file2Path) => {
-      const difference = compareFiles(file1Path, file2Path, format);
+describe.each(formatTbl)('output format: %s', (format, result) => {
+  test.each(extTbl)('input format: %s', (ext, file1Path, file2Path) => {
+    const difference = compareFiles(file1Path, file2Path, format);
 
-      expect(result).toEqual(difference);
-    });
+    expect(result).toEqual(difference);
   });
 });
