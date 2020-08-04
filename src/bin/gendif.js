56386f57ca60da9e13b129b8b5594ddf0138bc49
O Melchukova <rr26@mail.ru>
2020-08-04 20:44:17 +0300

    Carry over commanderOptions to gendif.js

--- src/bin/gendif.js
+++ src/bin/gendif.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env -S node --experimental-json-modules --no-warnings
+import program from 'commander';
+import packageJson from '../../package.json';
+import findDifference from '../../index.js';
 
-import useCommander from '../commanderOptions.js';
+program
+  .description(packageJson.description)
+  .version(packageJson.version, '-V, --version', 'output the version number')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    const difference = findDifference(filepath1, filepath2, options.format);
+    console.log(difference);
+  });
 
-useCommander();
+program.parse(process.argv);
