b7b1fd1b88e3d778d7ee93fe6cdc5a88c763a149
O Melchukova <rr26@mail.ru>
2020-08-07 06:44:43 +0300

    Carry over __dirname to module level; buildPath with relativePath

--- src/fileReader.js
+++ src/fileReader.js
@@ -2,14 +2,13 @@ import fs from 'fs';
 import { extname, dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const readFile = (path) => fs.readFileSync(path, 'utf-8');
 
 const getFormat = (path) => extname(path).substr(1);
 
-const buildPath = (name, ...directories) => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  return join(__dirname, '..', ...directories, name);
-};
+const buildPath = (relativePath) => join(__dirname, '..', relativePath);
 
 export { buildPath, readFile, getFormat };
