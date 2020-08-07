fb4fb38f81c59251e73bf870dc040f24455f8bb8
O Melchukova <rr26@mail.ru>
2020-08-07 06:10:46 +0300

    build paths in test, add case with default format, edit missprints

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,33 +1,33 @@
-import compareFiles from '../index.js';
+import genDiff from '../index.js';
 import { buildPath, readFile } from '../src/fileReader.js';
 
 const buildFixturePath = (filename) => buildPath(filename, '__fixtures__');
 
-const readFixtre = (name) => {
+const readFixture = (name) => {
   const path = buildFixturePath(name);
   return readFile(path);
 };
 
 const inputFormats = ['json', 'yml', 'ini'];
-const extTbl = inputFormats.map((ext) => {
-  const file1Path = buildFixturePath(`file1.${ext}`);
-  const file2Path = buildFixturePath(`file2.${ext}`);
-  return [ext, file1Path, file2Path];
-});
 
 describe('gendif', () => {
-  const stylishResult = readFixtre('stylishResult.txt');
-  const plainResult = readFixtre('plainResult.txt');
-  const jsonResult = readFixtre('jsonResult.txt');
+  const stylishResult = readFixture('stylishResult.txt');
+  const plainResult = readFixture('plainResult.txt');
+  const jsonResult = readFixture('jsonResult.txt');
 
-  test.each(extTbl)('input format: %s', (ext, file1Path, file2Path) => {
-    const stylishDifference = compareFiles(file1Path, file2Path, 'stylish');
+  test.each(inputFormats)('input format: %s', (ext) => {
+    const file1Path = buildFixturePath(`file1.${ext}`);
+    const file2Path = buildFixturePath(`file2.${ext}`);
+    const stylishDifference = genDiff(file1Path, file2Path, 'stylish');
     expect(stylishResult).toEqual(stylishDifference);
 
-    const plainDifference = compareFiles(file1Path, file2Path, 'plain');
+    const plainDifference = genDiff(file1Path, file2Path, 'plain');
     expect(plainResult).toEqual(plainDifference);
 
-    const jsonDifference = compareFiles(file1Path, file2Path, 'json');
+    const jsonDifference = genDiff(file1Path, file2Path, 'json');
     expect(jsonResult).toEqual(jsonDifference);
+
+    const defaultDifference = genDiff(file1Path, file2Path);
+    expect(defaultDifference).toEqual(stylishDifference);
   });
 });
